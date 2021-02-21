11d18ae63550b2345c51cfbd60598d45a9bbbd6e
Piterscl <you@example.com>
2021-02-21 17:13:28 -0300

    update

--- app.py
+++ app.py
@@ -1,27 +1,98 @@
-from flask import Flask, jsonify, request, render_template
+import os
+import datetime
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask import Flask, render_template, request, jsonify
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 from flask_cors import CORS
-from models import db
+from flask_jwt_extended import JWTManager, create_access_token, get_jwt_identity, jwt_required
+from models import db, User
+from dotenv import load_dotenv
 
+load_dotenv()
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
-app.config['DEBUG'] = True
-app.config['ENV'] = 'development'
+app.config['DEBUG'] = os.getenv('DEBUG')
+app.config['ENV'] = os.getenv('ENV')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://piter:password@host:3306/carwash_database'
-
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI')
+app.config['JWT_SECRET_KEY'] = os.getenv('JWT_SECRET_KEY')
 
 db.init_app(app)
+jwt = JWTManager(app)
 Migrate(app, db)
 CORS(app)
 manager = Manager(app)
-manager.add_command("db", MigrateCommand) #init, migrate, upgrade
+manager.add_command('db', MigrateCommand)  #init migrate y el upgrade
 
-@app.route('/')
-def root():
+@app.route("/")
+def main():
     return render_template('index.html')
+
+
+@app.route("/api/login", methods=['POST'])
+def login():
+    username = request.json.get('username')
+    password = request.json.get('password')
+
+    if not username: return jsonify({"msg": "nombre de es requerido"}), 400
+    if not password: return jsonify({"msg": "password is required"}), 400
+
+    user = User.query.filter_by(username=username).first()
+    if not user: return jsonify({"msg": "username/password are incorrect!!!"}), 400
+    
+
+    if not check_password_hash(user.password, password):
+        return jsonify({"msg": "username/password are incorrect!!!"}), 400
+    
+    expires = datetime.timedelta(days=3)
+
+    access_token = create_access_token(identity=user.id, expires_delta=expires)
+
+    data = {
+        "access_token": access_token,
+        "user": user.serialize()
+    }
+
+    return jsonify(data), 200
+
+@app.route("/api/Registro", methods=['POST'])
+def register():
+    username = request.json.get('username')
+    password = request.json.get('password')
+
+    if not username: return jsonify({"msg": "username is required"}), 400
+    if not password: return jsonify({"msg": "password is required"}), 400
+
+    user = User.query.filter_by(username=username).first()
+    if user: return jsonify({"msg": "username already exists!!!"}), 400
+    
+    user = User()
+    user.username = username
+    user.password = generate_password_hash(password)
+    user.save()
+
+    if not user: return jsonify({"msg": "Register Failed!!!"}), 400
+
+    expires = datetime.timedelta(days=3)
+
+    access_token = create_access_token(identity=user.id, expires_delta=expires)
+
+    data = {
+        "access_token": access_token,
+        "user": user.serialize()
+    }
+
+    return jsonify(data), 200
+
+
+@app.route("/api/perfil", methods=['GET'])
+@jwt_required()
+def profile():
+    id = get_jwt_identity()
+    user = User.query.get(id)
+    return jsonify(user.serialize()), 200
 
 if __name__ == '__main__':
     manager.run()
--- models.py
+++ models.py
@@ -3,19 +3,42 @@ db = SQLAlchemy()
 
 class User(db.Model):
     __tablename__ = 'users'
-    id = db.Column(db.Integer, primary_key=true) #Clave primaria de tabla
-    name = db.Column(db.String(100), nulleable=False)
-    last_name = db.Column(db.String(100), nulleable=False)
-    email = db.Column(db.String(100), nulleable=False, unique=True)
-    phone = db.Column(db.String(100), nulleable=False)
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(200), nullable=False)
+    password = db.Column(db.String(200), nullable=False)
+    email = db.Column(db.String(200), nullable=False, unique=True)
+    phone = db.Column(db.String(200), nullable=False)
 
     def serialize(self):
          return {
              "id": self.id,
-             "name": self.name,
-             "last_name": self.last_nae,
+             "username": self.name,
              "email": self.email,
              "phone": self.phone,
+         }
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(seld):
+        db.session.add(self)
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+class Servicios(db.Model):
+    __tablename__ = 'servicios'
+    id = db.Column(db.Integer, primary_key=true) #Clave primaria de tabla
+    label = db.Column(db.String(200))
+    price = db.Column(db.String(200))
+
+    def serialize(self):
+         return {
+             "id": self.id,
+             "label": self.label,
+             "price": self.price ,
          }
 
     def save(self):
